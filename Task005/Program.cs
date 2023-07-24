8f316b093bfc113d1cb72289f363f6596cecfb92
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-24 17:02:47 +0300

    рекурсия и факториал

---
+++ Task005/Program.cs
@@ -0,0 +1,31 @@
+﻿// Рекурсия 
+// Факториал = 5! = 5*4*3*2*1 или 5! = 5*4!
+
+//Функция факториал 
+// Варианn 1
+
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1; // 1! = 1, 0! = 1
+//     else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 5; i++)
+// {
+// Console.WriteLine(Factorial(i));
+// }
+
+// Вариант 2 (с показателем факториала)
+
+int Factorial(int n) // int или double = в зависимости от результата целое или десятичное число 
+{
+    if (n == 1) return 1; // 1! = 1, 0! = 1
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+
+
