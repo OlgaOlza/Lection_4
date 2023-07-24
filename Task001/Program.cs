626c91188a2b981cf66b0772ac216fc2bc4a73e0
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-24 13:21:21 +0300

    Пример двумерного массива

---
+++ Task001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пример двумерного массива 
+
+
+string[,] table = new string[2, 5];
+table[1, 2] = "слово";
+
+for (int rows = 0; rows < 2; rows++) // кол-во строк 
+{
+    for (int colums = 0; colums < 5; colums++) // кол-во столбцов
+    {
+        Console.WriteLine($"-{table[rows, colums]}-");
+    }
+}
